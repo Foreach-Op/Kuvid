3fcef8f2f874885d81e41201f4ba2d5aa2e6703c
ftokmak17 <ftokmak17@ku.edu.tr>
2020-12-03 00:45:14 +0300

    ObjectListener was updated.

--- src/Domain/Listeners/ObjectListener.java
+++ src/Domain/Listeners/ObjectListener.java
@@ -6,13 +6,9 @@ import Domain.Objects.Position;
 
 
 public interface ObjectListener {
-    //can we have a createMethod? GameScreen (or main GUI screen) will implement the createPanelListener.
-    //for every new object that will be added to gameScreen, we will call
-    //onCreate(GameObject obj).
-    // Then, the game screen will create panel of the object and add it to HashMap<GameObject, JPanel>.
+
     void onCreate(GameObject obj);
-    void addOnGameScreen(); // we will add also the GameObject as an input to these methods. Then gameScreen will find
-    //corresponding JPanel of each object and operate it.
+    void addOnGameScreen();
     void onLocationChange();
-    void onDestroy(); // in onDestroy we delete the object from HashMap for space efficiency.
+    void onDestroy();
 }
