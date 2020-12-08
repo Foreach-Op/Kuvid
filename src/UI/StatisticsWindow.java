ec14d6349d1737df277a403c621af8b11eb7b53d
Metehan <52951674+metehangelgi@users.noreply.github.com>
2020-12-08 20:35:47 +0300

    Jgoodies added to the project

--- src/UI/StatisticsWindow.java
+++ src/UI/StatisticsWindow.java
@@ -10,7 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 
-public class StatisticsWindow extends JFrame implements StaticWindowListener {
+public class StatisticsWindow extends JPanel implements StaticWindowListener {
     private JPanel panelMain;
 
     private JPanel panelGameInfo;
