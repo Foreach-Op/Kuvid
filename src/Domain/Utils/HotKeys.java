9d8b5538b06549353cbb520baf585442c5f062c5
Metehan <52951674+metehangelgi@users.noreply.github.com>
2020-12-13 10:03:52 +0300

    unneeded comments deleted

--- src/Domain/Utils/HotKeys.java
+++ src/Domain/Utils/HotKeys.java
@@ -9,22 +9,17 @@ public enum HotKeys {
     ROTATE_LEFT(KeyEvent.VK_Q), // SCROLL WHEEL
     SHOOT(KeyEvent.VK_SPACE), // LEFT CLICK
     PICK_ATOM(KeyEvent.VK_C), // SPACE
-    BLEND(KeyEvent.VK_B), // ALT + ATOM TYPE
+    BLEND(KeyEvent.VK_B),
     ATOM_TYPE1(KeyEvent.VK_1),
     ATOM_TYPE2(KeyEvent.VK_2),
     ATOM_TYPE3(KeyEvent.VK_3),
     ATOM_TYPE4(KeyEvent.VK_4),
+    // ALT + ATOM TYPE -> PICKING POWERUP
+
     PAUSE(KeyEvent.VK_P),
     RESUME(KeyEvent.VK_R),
-
     SAVE(KeyEvent.VK_S),
     LOAD(KeyEvent.VK_L);
-
-    // PICKING POWERUP, ONLY 1 - 2 - 3 - 4
-    //PICKPOWERUP_ALPHA(KeyEvent.VK_H),
-    //PICKPOWERUP_BETA(KeyEvent.VK_J),
-    //PICKPOWERUP_GAMMA(KeyEvent.VK_K),
-    //PICKPOWERUP_SIGMA(KeyEvent.VK_L);
 
 
     private int id;
