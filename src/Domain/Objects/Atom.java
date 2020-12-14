d460d33bc77fc07803da0406f6398b348342c4c8
Foreach-Op <oguztas1907@hotmail.com>
2020-12-14 14:25:53 +0300

    Atoms are faster now.

--- src/Domain/Objects/Atom.java
+++ src/Domain/Objects/Atom.java
@@ -8,7 +8,8 @@ public class Atom extends GameObject {
 
     public Atom(String subType,Position position,int angle) {
         super(FinalValues.ATOM, subType, position,angle,false);
-        setVelocityY(-getVelocityY());
+        setVelocityY(-getVelocityY()*getL()/50);
+        setVelocityX(getVelocityX()*getL()/50);
         setHeight(getL()/5);
         setWidth(getL()/5);
         //setRectangle(new Rectangle(position, widthCoef*L, heightCoef*L, angle));
