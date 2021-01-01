62600eee89e0f99486ee1b9073b82aca6d6aeef2
Metehan <52951674+metehangelgi@users.noreply.github.com>
2021-01-01 10:29:10 +0300

    Local-Mongo read-write ready (not save-load side)

--- src/Domain/UserFunctionalities/Local.java
+++ src/Domain/UserFunctionalities/Local.java
@@ -1,16 +1,29 @@
 package Domain.UserFunctionalities;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Local implements SaveLoadAdapter {
 
+
     @Override
     public JSONObject download(String filename) throws Exception {
-        return null;
+        JSONParser jsonParser = new JSONParser();
+        FileReader reader = new FileReader("save_files/"+filename+".json");
+        Object obj = jsonParser.parse(reader);
+        JSONObject json = (JSONObject) obj;
+        return json;
     }
 
     @Override
     public boolean upload(JSONObject json) throws Exception {
-        return false;
+        FileWriter file = new FileWriter("save_files/"+json.get("username")+".json");
+        file.write(json.toJSONString());
+        file.flush();
+        file.close();
+        return true;
     }
 }
