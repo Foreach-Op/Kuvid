4bae5a81343ed5ed49100cd13c1694d3d7bd6c93
Alper Kılınç <akilinc17@ku.edu.tr>
2021-01-17 19:46:58 +0300

    GameOverWindow update-fix

--- src/UI/MainTest.java
+++ src/UI/MainTest.java
@@ -5,7 +5,7 @@ import UI.Audio.AudioController;
 public class MainTest {
     public static void main(String[] args) {
         UIController uiController = UIController.GetInstance();
-        uiController.openGameOverWindow();
+        uiController.openHomeScreen();
 
         AudioController.GetInstance().onStart();
     }
