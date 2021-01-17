0faba2d5b555313c2603f63d6419961559586e06
Metehan <52951674+metehangelgi@users.noreply.github.com>
2021-01-17 11:36:25 +0300

    small rotating bugfix(still problem)

--- src/UI/ObjectPanel.java
+++ src/UI/ObjectPanel.java
@@ -56,8 +56,23 @@ public class ObjectPanel {
         final BufferedImage rotatedImage = new BufferedImage(w, h, bufferedImage.getType());
         final AffineTransform at = new AffineTransform();
         at.translate(w / 2, h / 2);
-        at.rotate(rads,width/2, height);
+        at.rotate(rads,width/2, height/2);
         at.translate(-bufferedImage.getWidth() / 2, -bufferedImage.getHeight() / 2);
+        final AffineTransformOp rotateOp = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
+        rotateOp.filter(bufferedImage,rotatedImage);
+        return rotatedImage;
+    }
+
+    //rotating objects
+    private BufferedImage rotateImage2() {
+        final double rads = Math.toRadians(rotation);
+        final double sin = Math.abs(Math.sin(rads));
+        final double cos = Math.abs(Math.cos(rads));
+        final int w = (int) Math.floor(bufferedImage.getWidth() * cos + bufferedImage.getHeight() * sin);
+        final int h = (int) Math.floor(bufferedImage.getHeight() * cos + bufferedImage.getWidth() * sin);
+        final BufferedImage rotatedImage = new BufferedImage(w, h, bufferedImage.getType());
+        final AffineTransform at = new AffineTransform();
+        at.rotate(Math.toRadians(rotation), bufferedImage.getWidth()/2, bufferedImage.getHeight()/2);
         final AffineTransformOp rotateOp = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
         rotateOp.filter(bufferedImage,rotatedImage);
         return rotatedImage;
@@ -83,7 +98,7 @@ public class ObjectPanel {
 
         else if(fallingType.equals("SPINNING")){
             if(type.equals("Molecule") && (subtype.equals(FinalValues.ALPHA) || subtype.equals(FinalValues.BETA))){
-                BufferedImage rotatedImage=rotateImage();
+                BufferedImage rotatedImage=rotateImage2();
                 newImage = rotatedImage.getScaledInstance(w, h, Image.SCALE_SMOOTH);
             }
         }
